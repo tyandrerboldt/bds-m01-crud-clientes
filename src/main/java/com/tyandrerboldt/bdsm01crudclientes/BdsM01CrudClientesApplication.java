600e04abd833738cb6ac7a33b5d64c443d36fe46
Tyandrer Boldt <tyandrer.boldt@gmail.com>
2020-10-01 09:01:01 -0300

    Project created

---
+++ src/main/java/com/tyandrerboldt/bdsm01crudclientes/BdsM01CrudClientesApplication.java
@@ -0,0 +1,13 @@
+package com.tyandrerboldt.bdsm01crudclientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BdsM01CrudClientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BdsM01CrudClientesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tyandrerboldt/bdsm01crudclientes/BdsM01CrudClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tyandrerboldt.bdsm01crudclientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BdsM01CrudClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
